9d6c3c6d6617f852d49120d8e9feb41f5b0acf5b
github-MaksimDolgonosov <max_air@bk.ru>
2023-10-05 16:55:15 +0300

    add: addition a herro

--- src/components/heroesAddForm/HeroesAddForm.js
+++ src/components/heroesAddForm/HeroesAddForm.js
@@ -1,5 +1,8 @@
-
-
+import { useFormik } from "formik";
+import { object, string } from 'yup';
+import { v4 as uuidv4 } from 'uuid';
+import { heroesFetched, heroesFetchingError } from "../../actions";
+import { useDispatch, useSelector } from "react-redux";
 // Задача для этого компонента:
 // Реализовать создание нового героя с введенными данными. Он должен попадать
 // в общее состояние и отображаться в списке + фильтроваться
@@ -11,37 +14,68 @@
 // данных из фильтров
 
 const HeroesAddForm = () => {
+    const heroes = useSelector(state => state.heroes)
+    const dispatch = useDispatch();
+    const formik = useFormik({
+        initialValues: {
+            name: "", description: "", element: ""
+        },
+        validationSchema: object({
+            name: string().min(3, "Минимум 3 буквы").required("Обязательное поле"),
+            description: string().min(5, "Минимум 5 букв").required("Обязательное поле"),
+            element: string().required("Выберете элемент героя"),
+        }),
+        onSubmit: values => {
+            const id = uuidv4();
+            const newHerro = { id: id, ...values, };
+            heroes.push(newHerro);
+            console.log(heroes);
+            dispatch(heroesFetched(heroes));
+
+        },
+    })
+
     return (
-        <form className="border p-4 shadow-lg rounded">
+        <form onSubmit={formik.handleSubmit} className="border p-4 shadow-lg rounded">
             <div className="mb-3">
                 <label htmlFor="name" className="form-label fs-4">Имя нового героя</label>
-                <input 
+                <input
                     required
-                    type="text" 
-                    name="name" 
-                    className="form-control" 
-                    id="name" 
-                    placeholder="Как меня зовут?"/>
+                    type="text"
+                    name="name"
+                    className="form-control"
+                    id="name"
+                    placeholder="Как меня зовут?"
+                    onChange={formik.handleChange}
+                    value={formik.values.name}
+                    onBlur={formik.handleBlur}
+                />
             </div>
-
+            {formik.errors.name && formik.touched.name ? <div>{formik.errors.name}</div> : null}
             <div className="mb-3">
                 <label htmlFor="text" className="form-label fs-4">Описание</label>
                 <textarea
                     required
-                    name="text" 
-                    className="form-control" 
-                    id="text" 
+                    name="description"
+                    className="form-control"
+                    id="text"
                     placeholder="Что я умею?"
-                    style={{"height": '130px'}}/>
+                    style={{ "height": '130px' }}
+                    onChange={formik.handleChange}
+                    value={formik.values.description}
+                    onBlur={formik.handleBlur} />
             </div>
 
             <div className="mb-3">
                 <label htmlFor="element" className="form-label">Выбрать элемент героя</label>
-                <select 
+                <select
                     required
-                    className="form-select" 
-                    id="element" 
-                    name="element">
+                    className="form-select"
+                    id="element"
+                    name="element"
+                    onChange={formik.handleChange}
+                    value={formik.values.element}
+                    onBlur={formik.handleBlur} as="select">
                     <option >Я владею элементом...</option>
                     <option value="fire">Огонь</option>
                     <option value="water">Вода</option>
